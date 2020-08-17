615f70bfb6b23036369ef1dd81f4c8e8aec9f7ce
danila27ru <gorelko.danila@gmail.com>
2020-08-17 16:42:01 +1000

    check dot inside area added

--- scripts/answer.php
+++ scripts/answer.php
@@ -1,4 +1,34 @@
 <?php
+// TODO: fix 404 not found error
+
+function checkData($x, $y, $r) {
+    return in_array($x, array(-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2)) &&
+        is_numeric($y) && ($y > -3 && $y < 5) &&
+        in_array($r, array( 1, 2, 3, 4, 5));
+}
+
+function atRectangle($x, $y, $r) {
+    if (($x <= 0) && ($x >= -$r/2) && ($y >= 0) && ($y <= $r)) return true;
+    else return false;
+}
+
+function atTriangle($x, $y, $r) {
+    if (($x >= 0) && ($x <= $r/2) && ($y >= 0) && ($y <= $r/2) && (sqrt($x**$x + $y**$y) <= $r**$r/2))
+        return true;
+    else return false;
+}
+
+function atQuarterCircle($x, $y, $r) {
+    if (($x <= 0) && ($y <= 0) && (($x**$x + $y**$y) <= $r)) return true;
+    else return false;
+}
+
+function checkCoordinates($x, $y, $r) {
+    if (atRectangle($x, $y, $r) || atTriangle($x, $y, $r) || atQuarterCircle($x, $y, $r)) {
+        return "Да";
+    }
+    else return "Нет";
+}
 
 @session_start();
 if (!isset($_SESSION["tableRows"])) $_SESSION["tableRows"] = array();
@@ -32,15 +62,4 @@ if (checkData($x, $y, $r)) {
 } else {
     http_response_code(400);
     return;
-}
-
-function checkData($x, $y, $r) {
-    return in_array($x, array(-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2)) &&
-        is_numeric($y) && ($y > -3 && $y < 5) &&
-        in_array($r, array( 1, 2, 3, 4, 5));
-}
-
-// TODO: add checkers for each zone
-function checkCoordinates($x, $y, $r) {
-
 }
